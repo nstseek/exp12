8fb41ea6d5ec07544225aa41d39863bd1bf89426
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-06-21 16:44:14 -0300

    display working fine

--- exp12.vhd
+++ exp12.vhd
@@ -37,18 +37,43 @@ entity exp12 is
 			  transistor : out STD_LOGIC_VECTOR(0 TO 3);
 			  rs : out STD_LOGIC;
 			  enable_lcd : out STD_LOGIC;
-			  lcd_data : out STD_LOGIC_VECTOR(0 TO 7)
+			  lcd_data : out STD_LOGIC_VECTOR(0 TO 7);
+			  switch : in STD_LOGIC_VECTOR(0 TO 7)
 			  );
 end exp12;
 
 architecture Behavioral of exp12 is
 
-	constant debug_clk : INTEGER := 1000; -- debug config, should be 1 for everything to work.
+	signal debug_clk : INTEGER := 1; -- debug config, should be 1 for everything to work.
 
-	constant basic_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := "00111000";
-	constant control_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := "00001111";
-	constant desloc_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := "00000110";
-	constant clear_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := "00000001";
+	constant basic_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := x"38";
+	constant control_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := x"0F";
+	constant desloc_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := x"06";
+	constant clear_conf_lcd : STD_LOGIC_VECTOR(0 TO 7) := x"01";
+	
+	constant R : STD_LOGIC_VECTOR(0 TO 7) := x"52";
+	constant a : STD_LOGIC_VECTOR(0 TO 7) := x"61";
+	constant f : STD_LOGIC_VECTOR(0 TO 7) := x"66";
+	constant e : STD_LOGIC_VECTOR(0 TO 7) := x"65";
+	constant l : STD_LOGIC_VECTOR(0 TO 7) := x"6C";
+	constant C : STD_LOGIC_VECTOR(0 TO 7) := x"43";
+	constant T : STD_LOGIC_VECTOR(0 TO 7) := x"54";
+	constant m : STD_LOGIC_VECTOR(0 TO 7) := x"6D";
+	constant p : STD_LOGIC_VECTOR(0 TO 7) := x"70";
+	constant min_r : STD_LOGIC_VECTOR(0 TO 7) := x"72";
+	constant min_t : STD_LOGIC_VECTOR(0 TO 7) := x"74";
+	constant u : STD_LOGIC_VECTOR(0 TO 7) := x"75";
+	constant ascii_0 : STD_LOGIC_VECTOR(0 TO 7) := x"30";
+	constant ascii_1 : STD_LOGIC_VECTOR(0 TO 7) := x"31";
+	constant ascii_2 : STD_LOGIC_VECTOR(0 TO 7) := x"32";
+	constant ascii_3 : STD_LOGIC_VECTOR(0 TO 7) := x"33";
+	constant ascii_4 : STD_LOGIC_VECTOR(0 TO 7) := x"34";
+	constant ascii_5 : STD_LOGIC_VECTOR(0 TO 7) := x"35";
+	constant ascii_6 : STD_LOGIC_VECTOR(0 TO 7) := x"36";
+	constant ascii_7 : STD_LOGIC_VECTOR(0 TO 7) := x"37";
+	constant ascii_8 : STD_LOGIC_VECTOR(0 TO 7) := x"38";
+	constant ascii_9 : STD_LOGIC_VECTOR(0 TO 7) := x"39";
+	constant miasbola : STD_LOGIC_VECTOR(0 TO 7) := x"BA";
 
 	signal clk_1khz_counter : INTEGER := 0;
 	signal clk_1khz : STD_LOGIC := '0';
@@ -67,6 +92,8 @@ architecture Behavioral of exp12 is
 	signal lcd_ready : STD_LOGIC := '0';
 	signal lcd_counter : INTEGER := 0;
 	
+	signal digit1_int : INTEGER := 0;
+	signal digit2_int : INTEGER := 0;
 	signal digit1 : STD_LOGIC_VECTOR(0 TO 7) := "00000000";
 	signal digit2 : STD_LOGIC_VECTOR(0 TO 7) := "00000000";
 	
@@ -87,22 +114,67 @@ begin
 	transistor <= "0000";
 	cs_ad <= '1';
 	clk_tmp121 <= clk_1khz;
-	led(0) <= lcd_data_signal(0);
-	led(1) <= lcd_data_signal(1);
-	led(2) <= lcd_data_signal(2);
-	led(3) <= lcd_data_signal(3);
-	led(4) <= lcd_data_signal(4);
-	led(5) <= lcd_data_signal(5);
-	led(6) <= lcd_data_signal(6);
-	led(7) <= rs_clk_signal;
+	
+	with digit1_int select
+	digit1 <=
+		ascii_0 when 0,
+		ascii_1 when 1,
+		ascii_2 when 2,
+		ascii_3 when 3,
+		ascii_4 when 4,
+		ascii_5 when 5,
+		ascii_6 when 6,
+		ascii_7 when 7,
+		ascii_8 when 8,
+		ascii_9 when 9,
+		x"58" when others;
+		
+	with digit2_int select
+	digit2 <=
+		ascii_0 when 0,
+		ascii_1 when 1,
+		ascii_2 when 2,
+		ascii_3 when 3,
+		ascii_4 when 4,
+		ascii_5 when 5,
+		ascii_6 when 6,
+		ascii_7 when 7,
+		ascii_8 when 8,
+		ascii_9 when 9,
+		x"58" when others;
+	
+	with switch select
+	debug_clk <=
+		1000 when "11100000",
+		100 when "11000000",
+		10 when "10000000",
+		1 when others;
+		
 	with enable_signal select 
 	clk_enable <=
 		clk_1khz when '1',
 		'0' when others;
+		
 	enable_lcd <= clk_enable;
 	
 	tmp_121_integer <= to_integer(unsigned(tmp_121_data));
 
+	proc_info: process(clk_1khz)
+	begin
+		if switch(1) = '1' then
+			led(0) <= lcd_data_signal(0);
+			led(1) <= lcd_data_signal(1);
+			led(2) <= lcd_data_signal(2);
+			led(3) <= lcd_data_signal(3);
+			led(4) <= lcd_data_signal(4);
+			led(5) <= lcd_data_signal(5);
+			led(6) <= lcd_data_signal(6);
+			led(7) <= rs_clk_signal;
+		else
+			led <= tmp_121_data;
+		end if;
+	end process;
+	
 	proc_1khz: process(clk_in)
 	begin
 	  if clk_in'event and clk_in = '1' then
@@ -155,7 +227,6 @@ begin
 				elsif tmp_121_counter > 310 and tmp_121_counter < 319 then
 					null;
 				else
-					write_to_lcd <= '1';
 					tmp_121_counter <= 0;
 				end if;
 			end if;
@@ -240,12 +311,38 @@ begin
 						lcd_data_signal <= desloc_conf_lcd;
 						lcd_counter <= lcd_counter + 1;
 					when 3 =>
-						rs_next_cycle <= '1';
+						rs_next_cycle <= '0';
 						lcd_data_signal <= clear_conf_lcd;
 						lcd_counter <= lcd_counter + 1;
-					when 4 =>
+					when 4 to 6 => 
+						lcd_counter <= lcd_counter + 1;
+						lcd_data_signal <= x"00";
+					when 7 =>
+						lcd_counter <= lcd_counter + 1;
+						rs_next_cycle <= '1';
+					when 8 =>
+						rs_next_cycle <= '1';
+						lcd_data_signal <= R;
+						lcd_counter <= lcd_counter + 1;
+					when 9 =>
+						rs_next_cycle <= '1';
+						lcd_data_signal <= a;
+						lcd_counter <= lcd_counter + 1;
+					when 10 =>
+						rs_next_cycle <= '1';
+						lcd_data_signal <= f;
+						lcd_counter <= lcd_counter + 1;
+					when 11 =>
+						rs_next_cycle <= '1';
+						lcd_data_signal <= a;
+						lcd_counter <= lcd_counter + 1;
+					when 12 =>
+						rs_next_cycle <= '1';
+						lcd_data_signal <= e;
+						lcd_counter <= lcd_counter + 1;
+					when 13 =>
 						rs_next_cycle <= '0';
-						lcd_data_signal <= "01010010";
+						lcd_data_signal <= l;						
 						lcd_counter <= 0;
 						lcd_ready <= '1';
 					when others => null;
